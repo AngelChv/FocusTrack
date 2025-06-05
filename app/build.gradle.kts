e2d3d5347656cd0db05a2dda01adb88d14577dd4
Angel Chicote <angel.chiveg@educa.jcyl.es>
2025-06-05 18:40:05 +0200

    Add Firebase Auth and configure Google Services
    This commit introduces Firebase Authentication to the project by:
    
    - Adding the `firebase-auth-ktx` dependency in `build.gradle.kts` and `libs.versions.toml`.
    - Updating `google-services.json` with new OAuth client configurations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,9 @@ dependencies {
     // JSON
     implementation(libs.kotlinx.serialization.json)
 
+    // Auth
+    implementation(libs.firebase.auth)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
